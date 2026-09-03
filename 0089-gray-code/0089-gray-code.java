a3dffe8fb0e9b471329859e22733bba08e0b99dc
chaitran_08 <chaitraneducation@gmail.com>
2026-09-03 23:04:44 +0530

    Time: 17 ms (6.77%), Space: 50.3 MB (99.57%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<Integer> grayCode(int n) {
+        int m = (int)(Math.pow(2,n));
 
-        List<Integer> ls = new ArrayList<>();
-        int m = (int)Math.pow(2,n);
         int [] arr = new int[m];
 
-        for(int i = 0 ; i < m ; i++)
+        for(int i = 0 ; i < m  ; i++)
         {
-            arr[i] = i ^ (i>>1);
+            arr[i] = i ^ (i >> 1);
         }
+        System.out.println(Arrays.toString(arr));
 
-        for(int i = 0 ; i < m ; i++)
+        List<Integer> ls = new ArrayList<>();
+
+        for(int el : arr)
         {
-            ls.add(arr[i]);
+            ls.add(el);
         }
         return ls;
     }
