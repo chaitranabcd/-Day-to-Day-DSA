7a0a9f03076e07261616e6bb4bcbb35f58c0954d
chaitran_08 <chaitraneducation@gmail.com>
2026-09-11 08:17:52 +0530

    Time: 1 ms (100%), Space: 48.2 MB (22.45%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,26 +1,47 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         
-        int [] res = new int[2];
+        // int [] res = new int[2];
+
+        // int n = nums.length;
+
+        // Map<Integer,Integer> map = new HashMap<>();
+
+        // for(int i = 0; i < n ; i++)
+        // {
+        //     map.put(nums[i],map.getOrDefault(nums[i], 0) + 1);
+        // }
+        // int j = 0;
+        // for(int i = 0 ; i < n ; i++)
+        // {
+        //     int x = map.get(nums[i]);
+        //     if(x == 1)
+        //     {
+        //         res[j++] = nums[i];
+        //     }
+        // }
+        // return res;
 
         int n = nums.length;
-
-        Map<Integer,Integer> map = new HashMap<>();
-
-        for(int i = 0; i < n ; i++)
-        {
-            map.put(nums[i],map.getOrDefault(nums[i], 0) + 1);
-        }
-        int j = 0;
+        int xorr  = 0;
         for(int i = 0 ; i < n ; i++)
         {
-            int x = map.get(nums[i]);
-            if(x == 1)
+            xorr ^= nums[i];
+        }
+        int rightSetBit = (xorr & xorr - 1) ^ xorr;
+        int b1 = 0, b2 = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if( (nums[i] & rightSetBit) != 0)
             {
-                res[j++] = nums[i];
+                b1 ^= nums[i];
+            }
+            else
+            {
+                b2 ^= nums[i];
             }
         }
-        return res;
+        return new int[] {b1,b2};
 
     }
 }
